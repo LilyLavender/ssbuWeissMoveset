114e252002e74de07455258c599fa1e386dbb451
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-05 20:02:45 -0500

    Better formatted pre status script

--- src/status/mod.rs
+++ src/status/mod.rs
@@ -122,26 +122,30 @@ unsafe extern "C" fn marth_specialn_main_loop(fighter: &mut L2CFighterCommon) ->
 #[status_script(agent = "marth", status = FIGHTER_STATUS_KIND_SPECIAL_N, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_PRE)]
 unsafe fn marth_specialn_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
 	fighter.sub_status_pre_SpecialNCommon();
-	StatusModule::init_settings(fighter.module_accessor, 
-	smash::app::SituationKind(*SITUATION_KIND_NONE),  
-	*FIGHTER_KINETIC_TYPE_UNIQ, 
-	GROUND_CORRECT_KIND_KEEP.into(), 
-	smash::app::GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_NONE), 
-	true, 
-	FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLAG.into(), 
-	*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_INT, 
-	FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLOAT.into(), 
-	0);
-	FighterStatusModuleImpl::set_fighter_status_data(fighter.module_accessor, 
-	false, 
-	*FIGHTER_TREADED_KIND_NO_REAC, 
-	false, 
-	false, 
-	false, 
-	(*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_N | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON | *FIGHTER_LOG_MASK_FLAG_SHOOT) as u64, 
-	FIGHTER_STATUS_ATTR_START_TURN.into(), 
-	FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_N.into(), 
-	0);
+	StatusModule::init_settings(
+		fighter.module_accessor, 
+		smash::app::SituationKind(*SITUATION_KIND_NONE),  
+		*FIGHTER_KINETIC_TYPE_UNIQ, 
+		GROUND_CORRECT_KIND_KEEP.into(), 
+		smash::app::GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_NONE), 
+		true, 
+		FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLAG.into(), 
+		*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_INT, 
+		FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLOAT.into(), 
+		0
+	);
+	FighterStatusModuleImpl::set_fighter_status_data(
+		fighter.module_accessor, 
+		false, 
+		*FIGHTER_TREADED_KIND_NO_REAC, 
+		false, 
+		false, 
+		false, 
+		(*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_N | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON) as u64, 
+		FIGHTER_STATUS_ATTR_START_TURN.into(), 
+		FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_N.into(), 
+		0
+	);
 	return 0.into();
 }
 
